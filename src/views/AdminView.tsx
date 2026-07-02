c2f64972bf4b4a9e98418289f611b95338891d45
gabrielescobar-droid <gabriel.escobar@decentralizedmasters.com>
2026-07-02 19:28:08 -0300

    v2

--- src/views/AdminView.tsx
+++ src/views/AdminView.tsx
@@ -44,6 +44,7 @@ export function AdminView() {
 
   const handleLogin = async () => {
     const provider = new GoogleAuthProvider();
+    provider.setCustomParameters({ prompt: 'select_account' });
     try {
       await signInWithPopup(auth, provider);
     } catch (err: any) {
